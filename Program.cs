1e1764dd88cf5400f74ea5dfc379347ac7c7d243
presidentofkazakhstan <alibekbekhan@gmail.com>
2019-04-14 20:46:43 +0600

    123

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student();
+            Console.Write("Ваши оценки(1-12): ");
+            student.MyMarks();
+            Console.WriteLine();
+
+
+            Console.Write("Кол-во пропусков: ");
+            student.CountOfPassLessons();
+            Console.WriteLine();
+
+
+
+            Console.Write("Кол-во опазданий: ");
+            student.CountOfLateLessons();
+            Console.Read();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class Student : IStudent
+    {
+        Random rand = new Random();
+        public int[] Mark = new int[5];
+
+
+        public void CountOfLateLessons()
+        {
+            int lateLesson  = rand.Next(1 , 5);
+            Console.Write(lateLesson);
+        }
+
+        public void CountOfPassLessons()
+        {
+            int passLesson = rand.Next(1, 5);
+            Console.Write(passLesson);
+        }
+
+        public void MyMarks()
+        {
+
+            for (int i = 0; i < Mark.Length; i++)
+            {
+                Mark[i] = rand.Next(1, 12);
+            }
+            for (int i = 0; i < Mark.Length; i++)
+            {
+                Console.Write(Mark[i] + " ");
+            }
+        }
+    }
+}
